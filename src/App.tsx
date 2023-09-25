c5de1f3008b74cb8e1521599940a0c79e1144edc
Robert Beekman <robert.beekman@rr-wfm.com>
2023-09-25 16:51:15 +0200

    Use vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import {useEffect, useState} from 'react'
+
+import './App.scss'
 
 function App() {
+    const [date, setDate] = useState(new Date());
 
-  const [date, setDate] = useState(new Date());
+    useEffect(() => {
+        const timer = setInterval(() => setDate(new Date()), 1000)
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    });
 
-  useEffect(() => {
-    const timer = setInterval(() => setDate(new Date()), 1000)
-    return function cleanup() {
-      clearInterval(timer)
-    }
-  });
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         R&R Time Challenge Template 😀
-        </p>
-        <b>{date.toUTCString()}</b>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    R&R Time Challenge Template 😀
+                </p>
+                <b>{date.toUTCString()}</b>
+            </header>
+        </div>
+    );
 }
 
-export default App;
+export default App
