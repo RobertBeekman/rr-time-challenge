dd625b6809a59d998544c3f510fdbd31d817c97f
Robert Beekman <robert.beekman@rr-wfm.com>
2023-10-04 16:43:49 +0200

    Really fix build

--- src/Finish.tsx
+++ src/Finish.tsx
@@ -1,17 +1,8 @@
 import Button from "react-bootstrap/Button";
-import {useEffect, useState} from "react";
 import {HighScore} from "./HighScore.tsx";
 import './Finish.scss'
 
 export function Finish(props: { advance: () => void, score: number }) {
-    const [date, setDate] = useState(new Date());
-    useEffect(() => {
-        const timer = setInterval(() => setDate(new Date()), 1000)
-        return function cleanup() {
-            clearInterval(timer)
-        }
-    });
-
     return <div className="finish-game-screen__BG">
         {/* <header className="finish-screen__time__container">
             <h1 className='finish-screen__time'>{date.toLocaleTimeString()}</h1>
